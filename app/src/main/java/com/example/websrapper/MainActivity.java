8d7e6dddbb0b4b116f800fa701f365f81b25b3ba
Matthias Volgger <stvolmat@bx.fallmerayer.it>
2021-06-03 10:34:57 +0200

    Programmstart

--- app/src/main/java/com/example/websrapper/MainActivity.java
+++ app/src/main/java/com/example/websrapper/MainActivity.java
@@ -1,33 +1,37 @@
 package com.example.websrapper;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.net.wifi.p2p.WifiP2pManager;
+import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.ArrayList;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        name = (EditText) findViewById(R.id.editTextTextPersonName);
-
-        final Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            String nam = name.getText().toString();
-            public void onClick(View v) {
-                System.out.println(nam);
-            }
-        });
-
 
     }
-
 }
